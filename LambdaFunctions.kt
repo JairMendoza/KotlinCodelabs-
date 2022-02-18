3fe188a6edf89bfc50e3074b5d576e1c6eefd183
jairbs <jairtactec@gmail.com>
2022-02-18 14:03:45 -0600

    Add LambdaFunctions.kt file

--- LambdaFunctions.kt
+++ LambdaFunctions.kt
@@ -3,4 +3,8 @@ fun main() {
     //La parte de abajo hace lo mismo que lo de arriba solo que esta abreviado.
     val triple: (Int) -> Int = { it * 3 }
     println(triple(5))
+    val peopleNames = listOf("Fred", "Ann", "Barbara", "Joe")
+    println(peopleNames.sorted())
+    println(peopleNames.sortedWith { str1: String, str2: String -> str1.length - str2.length })
+
 }
